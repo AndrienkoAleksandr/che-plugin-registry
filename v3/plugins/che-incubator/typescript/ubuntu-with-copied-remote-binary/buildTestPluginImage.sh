76751581d101e34a7836638c1910e298f5395274
Oleksandr Andriienko <oandriie@redhat.com>
2019-10-07 14:23:40 +0300

    Use newer node for typescript sidecar.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- v3/plugins/che-incubator/typescript/ubuntu-with-copied-remote-binary/buildTestPluginImage.sh
+++ v3/plugins/che-incubator/typescript/ubuntu-with-copied-remote-binary/buildTestPluginImage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t aandrienko/ubuntu-with-copied-remote-binary:latest .
+docker build --no-cache -t aandrienko/ubuntu-with-copied-remote-binary:latest .
 docker push aandrienko/ubuntu-with-copied-remote-binary:latest
